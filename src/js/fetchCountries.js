61c607fea30aea1288d85adcf2aa141a84d2d926
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-04-20 15:29:20 +0300

    hw

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-export default function fetchCountries(name) {
+export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
   return fetch(
     `${BASE_URL}${name}?fields=name,capital,flags,population,languages`
